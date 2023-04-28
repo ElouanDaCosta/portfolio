9297c7f47e671619722828bc18d14c74b369373c
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2023-04-28 22:12:38 +0200

    [FIX] the request, fetch all the data and render them, maybe need to review the architecture of the project

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import axios from "axios";
 import Menu from "../components/menu";
 import Footer from "../components/footer";
 
@@ -18,7 +17,7 @@ class Project extends Component {
   }
 
   componentDidMount() {
-    fetch("../data/projects.json")
+    fetch("/data/projects.json")
       .then(response => {
         if(!response.ok) {
           throw new Error("error " + response.status);
@@ -26,61 +25,62 @@ class Project extends Component {
         return response.json();
       })
       .then(data => {
-        console.log(data);
         const urlParams = new URLSearchParams(window.location.search);
         const id = urlParams.get('id');
-        const project = data.find(project => project.id === id);
-        console.log(project);
-        this.setState({
-          projectTitle: project.title,
-          projectDescription: project.description,
-          projectImage: project.image,
-          projectPresentation: project.presentation,
-          projectKeywords: project.keywords,
-          projectExample: project.example,
-          projectGithub: project.github,
-        });
+        const project = data.find(item => item.id === Number(id));
+        //console.log(project.project_title);
+        if (project) {
+          this.setState({
+            projectTitle: project.project_title,
+            projectDescription: project.project_description,
+            projectImage: project.project_image,
+            projectPresentation: project.project_presentation,
+            projectKeywords: project.project_keywords,
+            projectExample: project.project_example,
+            projectGithub: project.project_github,
+          });
+        }
       })
       .catch(error => {
         console.log("error " + error);
       });
   }
 
-  render(projectImage, projectPresentation, projectKeywords, projectTitle, projectDescription, projectExample, projectGithub) {
+  render() {
     return (
       <React.Fragment>
         <Menu />
         <section className="project-header">
           <div className="project-header__title">
-            <h1>{projectPresentation}</h1>
+            <h1>{this.state.projectTitle}</h1>
           </div>
           <div className="project-header__keywords">
             <div className="project-header__keywords__date">
               <p>2020</p>
             </div>
             <div className="project-header__keywords__keywords">
-              <p>{projectKeywords}</p>
+              <p>{this.state.projectKeywords}</p>
             </div>
           </div>
         </section>
         <section className="projectPresentation">
           <div className="projectPresentation__image">
-            <img src={projectImage} alt=""/>
+            <img src={`../assets/project_images/${this.state.projectImage}`} alt=""/>
           </div>
           <div className="projectPresentation__text">
             <div className="projectPresentation__text__title">
-              <h2>{projectTitle}</h2>
+              <h2>{this.state.projectTitle}</h2>
             </div>
             <div className="projectPresentation__text__secondtitle">
               <h3></h3>
             </div>
             <div className="projectPresentation__text__description">
-              <p>{projectDescription}</p>
+              <p>{this.state.projectDescription}</p>
             </div>
           </div>
           <div className="projectPresentation__example">
             <div className="projectPresentation__example__image">
-              <img src={projectExample} alt=""/>
+              <img src={this.state.projectExample} alt=""/>
             </div>
           </div>
         </section>
