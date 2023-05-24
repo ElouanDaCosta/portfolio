9f4f7183334597e5a60a53209993c39fe7985109
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2023-05-24 15:13:25 +0200

    feat: try to change to a new version of the portfolio, change all the colors and design

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,18 +5,14 @@ function Header ({ animation }) {
     <header className="header" id="home">
       <section className="header__text">
         <div className="header__text__title">
-          <h1 className="header__text__name">Hi, I'm <span className="header__text__name__highlight">Elouan</span>, web developper and Epitech student</h1>
+          <h1 className="header__text__title__highlight">Hi, my name is </h1>
+          <h2 className="header__text__name"><span className="header__text__name__highlight">Elouan</span>, web developper and Epitech student</h2>
         </div>
         <div className="header__text__paragraph">
           <p>Passionate about web development, I am currently studying at Epitech, a school of computer science and digital technology. I am looking for an internship in web development.</p>
           
         </div>
       </section>
-      <div
-      onClick={animation}
-      className="header__image">
-        <img src={require("../assets/me.jpg")} alt="Elouan Da Costa Peixoto" />
-      </div>
     </header>
   )
 }
