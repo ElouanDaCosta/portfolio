a8965cacd71e9ba1b8d7d9fe1bfce2afd3a52d03
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2023-05-27 22:16:36 +0200

    feat: add react-responsive and hamburger-react

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import MediaQuery from "react-responsive";
+import { Divide as Hamburger } from 'hamburger-react'
 import { useNavigate } from 'react-router-dom';
 
 function Menu () {
   const navigate = useNavigate();
-
+  const [isOpen, setOpen] = useState(false);
   return (
-    <nav className="menu">
-      <ul className="menu__list">
-        <li className="menu__list__item" onClick={() => navigate('/')}><span>01.</span>About</li>
-        <li className="menu__list__item" onClick={() => navigate('/about')}><span>02.</span>Experience</li>
-        <li className="menu__list__item" onClick={() => navigate('/projects')}><span>03.</span>Work</li>
-        <li className="menu__list__item" onClick={() => navigate('/contact')}><span>04.</span>Contact</li>
-      </ul>
-    </nav>
+    <React.Fragment>
+      <MediaQuery maxWidth={1024}>
+        <Hamburger isOpen={isOpen} setOpen={setOpen} />
+        {isOpen && (
+          <nav className="menu">
+            <ul className="menu__list">
+              <li className="menu__list__item" onClick={() => navigate('/')}><span>01.</span>About</li>
+              <li className="menu__list__item" onClick={() => navigate('/about')}><span>02.</span>Experience</li>
+              <li className="menu__list__item" onClick={() => navigate('/projects')}><span>03.</span>Work</li>
+              <li className="menu__list__item" onClick={() => navigate('/contact')}><span>04.</span>Contact</li>
+            </ul>
+          </nav>
+        )}
+      </MediaQuery>
+      <nav className="menu">
+        <ul className="menu__list">
+          <li className="menu__list__item" onClick={() => navigate('/')}><span>01.</span>About</li>
+          <li className="menu__list__item" onClick={() => navigate('/about')}><span>02.</span>Experience</li>
+          <li className="menu__list__item" onClick={() => navigate('/projects')}><span>03.</span>Work</li>
+          <li className="menu__list__item" onClick={() => navigate('/contact')}><span>04.</span>Contact</li>
+        </ul>
+      </nav>
+    </React.Fragment>
   )
 }
 
